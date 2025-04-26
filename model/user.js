f82efa1af4dbc7263f9bddba2fbe22d1e4eb2400
Albin-gits <albinjoseph2015@gmail.com>
2025-04-26 10:34:46 +0530

    first commit

--- model/user.js
+++ model/user.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  signupDate: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("user", userSchema);
